312d6b8519783140125cf902c0f82e85d78c5f28
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-02-22 18:17:08 +0200

    who-watch-cmp

--- js/cmps/who-watch-cmp.js
+++ js/cmps/who-watch-cmp.js
@@ -1,15 +1,18 @@
 Vue.component('who-watch', {
     props: ['user'],
     template: `
-        <div class="who-watch">
+        <div class="who-watch" v-bind:class="{'hide': !user.isEnable}">
             <img v-bind:src="imgUrl" v-on:click="selectUser()"/>
             <h3>{{user.name}}</h3>
             <button v-on:click="deleteUser()">Delete</button>
+            <select>
+                {{formatedOptions}}
+            </select>
         </div>
     `,
     data() {
         return {
-
+            // isEnable: false
         }
     },
     methods: {
@@ -18,16 +21,21 @@ Vue.component('who-watch', {
             this.$emit('deleted', this.user.id);
         },
         selectUser() {
-            console.log(this.user.shows);
+            // console.log(this.user.shows);
+            this.$emit('selected', this.user);
         }
     },
     computed: {
         imgUrl() {
             return `https://api.adorable.io/avatars/285/${this.user.name}.png`;
+        },
+        formatedOptions() {
+            return `<option value="Ariel">Ariel</option>`;
+            // return '1111';
         }
     },
     created() {
         console.log('who-watch component created!');
-
+        // this.isEnable = this.user.isEnable
     }
 })
--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ window.vueApp = new Vue({
     el: '#app',
     data: {
         timeStamp: Date.now() - 12000,
-        users: [{ id: 1, name: 'user1', shows: ['show1', 'show2'] },
-        { id: 2, name: 'user2', shows: ['show1', 'show3'] },
-        { id: 3, name: 'user3', shows: ['show2'] },
-        { id: 4, name: 'user4', shows: ['show3', 'show4'] },
-        { id: 5, name: 'user5', shows: ['show1', 'show3', 'show5'] }]
+        users: [{ id: 1, name: 'user1', shows: ['show1', 'show2'], isEnable: true },
+        { id: 2, name: 'user2', shows: ['show1', 'show3'], isEnable: true },
+        { id: 3, name: 'user3', shows: ['show2'], isEnable: true },
+        { id: 4, name: 'user4', shows: ['show3', 'show4'], isEnable: true },
+        { id: 5, name: 'user5', shows: ['show1', 'show3', 'show5'], isEnable: true }],
     },
     methods: {
         updatetimeStamp(newTimeStamp) {
@@ -23,14 +23,27 @@ window.vueApp = new Vue({
             var user = {
                 id: this.findNextId(),
                 name: userName,
-                shows: shows.split(',')
+                shows: shows.split(','),
+                isEnable: true
             }
             this.users.push(user);
+        },
+        selectUser(user) {
+            console.log(user);
+            this.users.map((item) => {
+                item.isEnable = false;
+            });
+            user.isEnable = true;
         },
         deleteUser(userId) {
             // console.log(userId);
             this.users = this.users.filter((item) => {
                 return item.id != userId;
+            });
+        },
+        switchUser() {
+            this.users.map((item) => {
+                item.isEnable = true;
             });
         },
         findNextId() {
